fe8bd781f95c066b0594ba295339edb9d02ef182
aj07256768-sys <aj07256768@gmail.com>
2026-07-29 10:09:11 +0300

    done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,4 +4,12 @@ import { api } from "../api/client";
 import RecipeCard from "../components/RecipeCard";
 export default function Home() {
   const [recipes, setRecipes] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    api
+      .listRecipes()
+      .then((data) => setRecipes(data.recipes.slice(0, 6)))
+      .catch(() => setRecipes([]))
+      .finally(() => setLoading(false));
+  }, []); 
